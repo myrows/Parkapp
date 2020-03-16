9c672da2d582e3f819fd6eddce1ef08bbfa8ad0b
myrows <milenio204@hotmail.com>
2020-03-16 19:56:48 +0100

    changes

--- Parkapp/app/src/main/java/com/example/parkapp/MiAparcamientoActivity.java
+++ Parkapp/app/src/main/java/com/example/parkapp/MiAparcamientoActivity.java
@@ -9,7 +9,7 @@ public class MiAparcamientoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_mi_aparcamiento);
+       //setContentView(R.layout.activity_mi_aparcamiento);
 
 
 
