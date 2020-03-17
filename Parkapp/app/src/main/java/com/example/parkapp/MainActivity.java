1dc8f1c90b5dd4d3e622ca1e72930c408690f46f
myrows <milenio204@hotmail.com>
2020-03-17 19:47:30 +0100

    [ADD] navigation in fragments

--- Parkapp/app/src/main/java/com/example/parkapp/MainActivity.java
+++ Parkapp/app/src/main/java/com/example/parkapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.parkapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -10,18 +14,33 @@ import com.example.parkapp.recyclerview.zona.ZonaFragment;
 import com.example.parkapp.recylcerview.aparcamiento.AparcamientoFragment;
 import com.example.parkapp.retrofit.model.Aparcamiento;
 import com.example.parkapp.retrofit.model.Zona;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity implements ZonaFragment.OnListFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements ZonaFragment.OnListFragmentInteractionListener, AparcamientoFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_zonas, R.id.navigation_maps, R.id.navigation_aparcamiento)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
 
     @Override
     public void onListFragmentInteraction(Zona item) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(Aparcamiento item) {
 
     }
 }
