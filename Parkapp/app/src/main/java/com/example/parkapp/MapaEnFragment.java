5ee5bbbfe3bd6f6c9a19ad4ad6b531d6d1d5ab16
Ricardo <mejiasdoradoricardo@gmail.com>
2020-03-18 13:16:00 +0100

    Mapa funcional con marcadores rojos

--- Parkapp/app/src/main/java/com/example/parkapp/MapaEnFragment.java
+++ Parkapp/app/src/main/java/com/example/parkapp/MapaEnFragment.java
@@ -1,18 +1,19 @@
 package com.example.parkapp;
 
-import androidx.annotation.RequiresApi;
-import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
-import android.os.Build;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.example.parkapp.common.MyApp;
-import com.example.parkapp.recylcerview.aparcamiento.DetalleAparcamientoActivity;
+import com.example.parkapp.R;
 import com.example.parkapp.retrofit.generator.ServiceGenerator;
 import com.example.parkapp.retrofit.model.Zona;
 import com.example.parkapp.retrofit.service.ParkappService;
@@ -20,53 +21,47 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MapaEnFragment extends SupportMapFragment implements OnMapReadyCallback {
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MapaEnFragment extends Fragment implements OnMapReadyCallback {
     ParkappService service;
-    private GoogleMap mMap;
-    List<Zona> listadoZonas;
+    GoogleMap mMap;
+    List<Zona> listado;
 
-    /*@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_mapa_en_fragment);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }*/
+    public MapaEnFragment() {
+        // Required empty public constructor
+    }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = super.onCreateView(inflater, container, savedInstanceState);
-        v.setClipToOutline(true);
+        View v = inflater.inflate(R.layout.activity_mapa_en_fragment, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
 
         return v;
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -77,46 +72,25 @@ public class MapaEnFragment extends SupportMapFragment implements OnMapReadyCall
             @Override
             public void onResponse(Call<List<Zona>> call, Response<List<Zona>> response) {
                 if(response.isSuccessful()){
-                    listadoZonas = response.body();
+                    listado = response.body();
 
-                    for(int i = 0; i<listadoZonas.size(); i++){
-
+                    for(int i = 0; i<listado.size(); i++){
                         //SI NO ESTA VACIO
-                        List<Double> LatIng =null;
-                        LatIng.add(listadoZonas.get(i).getLatitud());
-                        LatIng.add((listadoZonas.get(i).getLongitud()));
-                        if(!LatIng.isEmpty()){
+                        if(listado.get(i).getLatitud() != null && listado.get(i).getLongitud() != null) {
                             Marker m = mMap.addMarker(new MarkerOptions()
-                                    .position(new LatLng(listadoZonas.get(i).getLatitud(),listadoZonas.get(i).getLongitud()))
+                                    .position(new LatLng(listado.get(i).getLatitud(),listado.get(i).getLongitud()))
                                     .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
-                                    .title(listadoZonas.get(i).getNombre()));
+                                    .title(listado.get(i).getNombre()));
 
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(37.389170, -5.984487)));
-
-
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.3828300, -5.9731700),12.0f));
 
                         }
                     }
-                    mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-                        @Override
-                        public boolean onMarkerClick(Marker marker) {
-                            /*Intent i = new Intent(MapaEnFragment.this,
-                                    DetalleAparcamientoActivity.class);
-                            startActivity(i);*/
-
-                            return false;
-                        }
-                    });
-
-
                 }
-
             }
-
-
             @Override
             public void onFailure(Call<List<Zona>> call, Throwable t) {
-                Toast.makeText(MyApp.getContext(), "Error al realizar la petición", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "Error al realizar la petición", Toast.LENGTH_SHORT).show();
             }
 
         });
