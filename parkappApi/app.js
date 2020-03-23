6d84d5bffc5c2295030c54f421f7527827dae06d
myrows <milenio204@hotmail.com>
2020-03-23 22:26:57 +0100

    [UPD] api CORS

--- parkappApi/app.js
+++ parkappApi/app.js
@@ -88,6 +88,14 @@ function normalizePort(val) {
     return false
   }
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
